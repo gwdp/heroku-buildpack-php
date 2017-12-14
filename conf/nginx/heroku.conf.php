f8a16e8a5065ceb59f78b605790b21c9b4188125
Gabriel Debes <gabriel.wady@gmail.com>
2017-12-14 13:37:09 -0800

    fix indent

--- conf/nginx/heroku.conf.php
+++ conf/nginx/heroku.conf.php
@@ -2,15 +2,15 @@ http {
     include       mime.types;
     default_type  application/octet-stream;
 
-    #log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-    #                  '$status $body_bytes_sent "$http_referer" '
-    #                  '"$http_user_agent" "$http_x_forwarded_for"';
+    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" "$http_x_forwarded_for"';
 
     #access_log  logs/access.log  main;
 
     log_format combinedVHosts '$host - $http_x_forwarded_for - $remote_user [$time_local] "$request" '
-                              '$status $body_bytes_sent "$http_referer" '
-                              '"$http_user_agent" $request_time';
+                      '$status $body_bytes_sent "$http_referer" '
+                      '"$http_user_agent" $request_time';
 
     sendfile        on;
     #tcp_nopush     on;
